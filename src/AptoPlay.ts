e6330d13dad4fa03285b98e00845c52beaaf8c4b
Phantola <devmonarch0115@gmail.com>
2023-01-31 22:53:57 +0900

    Add google oauth register, Define custom error obj

--- src/AptoPlay.ts
+++ src/AptoPlay.ts
@@ -1,5 +1,9 @@
 import axios from 'axios';
-import { parseObjectPascalToCamel } from './utils';
+import {
+  generateErrorObject,
+  getGoogleProfileByAccessToken,
+  parseObjectPascalToCamel
+} from './utils';
 
 export class AptoPlay {
   private titleId: string;
@@ -9,7 +13,7 @@ export class AptoPlay {
   constructor(titleId: string, xSecretKey: string) {
     this.titleId = titleId;
     this.xSecretKey = xSecretKey;
-    this.baseUrl = `https://${this.titleId}.playfabapi.com`;
+    this.baseUrl = `https://${titleId}.playfabapi.com`;
   }
 
   public getTitleId(): string {
@@ -24,47 +28,61 @@ export class AptoPlay {
     return this.baseUrl;
   }
 
-  public async registerUser(
-    email: string,
-    password: string,
-    callback?: (data: any) => any
-  ) {
-    const res = await axios.post(this.baseUrl + '/Client/RegisterPlayFabUser', {
-      TitleId: this.titleId,
-      Email: email,
-      Password: password,
-      Username: new Date().getTime().toString() // Username: PlayFab username for the account (3-20 characters)
-    });
+  public async registerUser(email: string, password: string) {
+    try {
+      const res = await axios.post(
+        this.baseUrl + '/Client/RegisterPlayFabUser',
+        {
+          TitleId: this.titleId,
+          Email: email,
+          Password: password,
+          Username: new Date().getTime().toString() // Username: PlayFab username for the account (3-20 characters)
+        }
+      );
 
-    const data = parseObjectPascalToCamel(res.data);
+      const data = parseObjectPascalToCamel(res.data);
 
-    if (callback) {
-      callback(data);
+      return parseObjectPascalToCamel(data);
+    } catch (err: any) {
+      throw generateErrorObject('PLAYFAB_REGISTER_WITH_EMAIL_ERROR', err);
     }
-
-    return parseObjectPascalToCamel(data);
   }
 
-  public async login(
-    email: string,
-    password: string,
-    callback?: (data: any) => any
-  ) {
-    const res = await axios.post(
-      this.baseUrl + '/Client/LoginWithEmailAddress',
-      {
-        TitleId: this.titleId,
-        Email: email,
-        Password: password
-      }
-    );
+  public async login(email: string, password: string) {
+    try {
+      const res = await axios.post(
+        this.baseUrl + '/Client/LoginWithEmailAddress',
+        {
+          TitleId: this.titleId,
+          Email: email,
+          Password: password
+        }
+      );
 
-    const data = parseObjectPascalToCamel(res.data);
+      const data = parseObjectPascalToCamel(res.data);
 
-    if (callback) {
-      callback(data);
+      return parseObjectPascalToCamel(data);
+    } catch (err: any) {
+      throw generateErrorObject('PLAYFAB_LOGIN_WITH_EMAIL_ERROR', err);
     }
+  }
 
-    return parseObjectPascalToCamel(data);
+  public async registerWithGoogleAccount(accessToken: string) {
+    const email = await getGoogleProfileByAccessToken(accessToken);
+
+    try {
+      const playFabRes = await axios.post(
+        `${this.baseUrl}/Client/LoginWithGoogleAccount`,
+        {
+          TitleId: this.titleId,
+          CreateAccount: true,
+          AccessToken: accessToken
+        }
+      );
+
+      return parseObjectPascalToCamel({ ...playFabRes, email });
+    } catch (err: any) {
+      throw generateErrorObject('PLAYFAB_GOOGLE_SOCIAL_REGISER_ERROR', err);
+    }
   }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,25 @@
+import axios from 'axios';
+
+export function generateErrorObject(
+  errorTitle: string,
+  errorObject: any
+): CustomError {
+  const error: CustomError = {
+    errorTitle,
+    errorObject: {}
+  };
+
+  if (axios.isAxiosError(errorObject)) {
+    error.errorObject['code'] = errorObject.code;
+    error.errorObject['response'] = errorObject.response;
+    error.errorObject['message'] = errorObject.message;
+  } else {
+    error.errorObject = errorObject;
+  }
+
+  return error;
+}
+
 export function parseObjectPascalToCamel(object: { [k: string]: any }): {
   [k: string]: any;
 } {
@@ -11,4 +33,23 @@ export function parseObjectPascalToCamel(object: { [k: string]: any }): {
   }
 
   return newObject;
+}
+
+export async function getGoogleProfileByAccessToken(
+  accessToken: string
+): Promise<string> {
+  try {
+    const userInfoRes = await axios.get(
+      'https://www.googleapis.com/oauth2/v3/userinfo',
+      {
+        params: {
+          access_token: accessToken
+        }
+      }
+    );
+
+    return userInfoRes.data.email;
+  } catch (err: any) {
+    throw generateErrorObject('GOOGLE_GET_PROFILE_ERROR', err);
+  }
 }
